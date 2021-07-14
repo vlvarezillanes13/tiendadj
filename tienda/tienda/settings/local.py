664ef003508d20ccf41555edfd9870bf23705a64
Vicente <v.lvarez.illanes@gmail.com>
2021-07-14 16:58:46 -0400

    comentar email

--- tienda/tienda/settings/local.py
+++ tienda/tienda/settings/local.py
@@ -27,9 +27,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = get_secret('EMAIL')
-EMAIL_HOST_PASSWORD = get_secret('PASS_EMAIL')
-#PORT UBUNTU
-EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = get_secret('EMAIL')
+#EMAIL_HOST_PASSWORD = get_secret('PASS_EMAIL')
+#########PORT UBUNTU
+#EMAIL_PORT = 587
