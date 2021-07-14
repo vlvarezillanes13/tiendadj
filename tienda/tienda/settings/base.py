2e3607fe26d96778c6eb8525f4a3911b684d6e1a
Vicente <v.lvarez.illanes@gmail.com>
2021-07-14 16:57:51 -0400

    template

--- tienda/tienda/settings/base.py
+++ tienda/tienda/settings/base.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'tienda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.child('templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
